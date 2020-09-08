c44147c690261259b14fabc6579e2b05a0688499
Diego333-ms <66188985+Diego333-ms@users.noreply.github.com>
2020-09-08 13:57:33 -0300

    Add files via upload

---
+++ pc_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+instalador() {
+setterm -foreground green
+printf "["
+setterm -foreground white
+printf "+"
+setterm -foreground green
+printf "]"
+setterm -foreground cyan
+printf " Instalando Recursos...\n\n"
+sleep 2
+setterm -foreground white
+apt-get update
+apt install python
+apt install python3-pip
+apt install curl
+pip3 install requests
+verificar
+setterm -foreground green
+printf "["
+setterm -foreground white
+printf "+"
+setterm -foreground green
+printf "]"
+setterm -foreground cyan
+printf " Instalação Concluída com Sucesso.\n"
+}
+
+verificar() {
+if [[ -e python3 ]]; then
+echo ""
+ else
+command -v python3 > /dev/null 2>&1 || { printf >&2 "Não foi possivel instalar o python."; exit 1; }
+fi
+
+if [[ -e pip3 ]]; then
+echo ""
+ else
+command -v pip3 > /dev/null 2>&1 || { printf >&2 "Não foi possivel instalar o Pip."; exit 1; }
+fi
+
+if [[ -e curl ]]; then
+echo ""
+ else
+command -v curl > /dev/null 2>&1 || { printf >&2 "Não foi possivel instalar o Curl."; exit 1; }
+fi
+}
+
+instalador
